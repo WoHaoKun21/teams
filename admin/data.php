e134357073fb32479a6e557ea7055f31e313152e
wohaokun21 <609914130@qq.com>
2021-11-17 14:36:38 +0800

    fix:修改了不能翻页的问题

--- admin/data.php
+++ admin/data.php
@@ -1,7 +1,7 @@
 <?php
 if($_SERVER['REQUEST_METHOD']=='POST'){
 	include("conn.php");
-	$rs = mysql_query($conn,"select * from message");
+	$rs = mysql_query("select * from message");
 	$num = mysql_num_rows($rs);
 	echo $num;
 	mysql_close($conn);
